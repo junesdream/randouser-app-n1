e2b0719e1c93d97f2363a04847517005f22c43df
June <norisori@gmail.com>
2022-02-01 16:12:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import "./App.css";
+import Button from "./components/Button";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const [userData, setUserData] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [activeUser, setActiveUser] = useState(false);
+	const [activeLink, setActiveLink] = useState(0);
+
+	const onClickHandler = () => {
+    setActiveLink(0);
+		setLoading(true);
+		axios.get("https://randomuser.me/api/")
+    .then((response) => {
+			console.log(response.data.results);
+			setUserData(response.data.results);
+		}).catch((error) => {
+      console.log(error);
+      setLoading(true);
+    }).finally(() => {
+      setLoading(false);
+      setActiveUser(true);
+    })
+	};
+const icons = [
+  "fas fa-user fa-4x",
+  "fas fa-envelope fa-4x",
+  "fas fa-calender-alt fa-4x",
+  "fas fa-map-marker fa-4x",
+  "fas fa-phone fa-4x",
+  "fas fa-lock fa-4x"
+];
+
+const PhraseGenerator = ({user}) => {
+  const phrases = [
+    `Hi my name is ${user.name.first} ${user.name.last}`,
+    `My email adresse is ${user.email}`,
+    `I was born on ${user.dob.date.slice(0,10)}`,
+    `My Country is ${user.location.country}`,
+    `My Phone nummer ${user.phone.first}`,
+    `My password is ${user.login.password}`,
+  ]
+  return <h1> {phrases[activeLink]} </h1>
 }
+
+const activeLinkHandler = (index) => {
+	setActiveLink(index);
+};
+
+const style = {
+	color: "#FF00CC",
+};
+
+	return (
+		<div className="App">
+			<span className="app-title">Random User Generator App</span>
+			<Button isActive={activeUser} clicked={onClickHandler} />
+      {loading ? (
+        <h1>Loading..</h1>
+
+      ) : (
+        <div className="app__user">
+          {userData.map((user, index ) => {
+            return (
+              <Fragment key ={user.cell}>
+                <img src={user.picture.large} alt="#" />
+                <PhraseGenerator user={user}>
+
+                </PhraseGenerator>
+                <div className="app__icons">
+                  {icons.map((icon, index) => {
+                    return(
+                      <i className={icon} key={index} onMouseEnter={() => activeLinkHandler(index)}  style={activeLink === index ? style : null}></i>
+                    )
+                  })}
+                </div>
+              </Fragment>
+            )
+          })}
+        </div>
+      )}
+		</div>
+	);
+};
 
 export default App;
